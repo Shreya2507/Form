bd8f64c3af593661d9099181cf33ee49adfedad3
Shreya Mathur <smathur245@gmail.com>
2025-02-06 18:55:44 +0530

    updated for deploy

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const serverless = require("serverless-http");
 
 const app = express();
 app.use(cors());
@@ -14,9 +13,6 @@ app.get("/api", (req, res) => {
     });
 });
 
-// app.listen(3000, () => {
-//     console.log("Server running on port 3000");
-// });
-
-app.use("/.netlify/backend/app", router);
-module.exports.handler = serverless(app);
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
